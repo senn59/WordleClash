6b404b6b7d046a06069624d764fafa33dc029ab9
senn59 <74777070+senn59@users.noreply.github.com>
2024-04-07 10:15:27 +0200

    add more tests for word feedback

--- WordleClash.Tests/WordFeedbackTests.cs
+++ WordleClash.Tests/WordFeedbackTests.cs
@@ -24,6 +24,57 @@ public class Tests
         ];
         Assert.That(res.Feedback, Is.EqualTo(expectedResult));
     }
+    
+    [Test]
+    public void CorrectLetterMultipleOccurences()
+    {
+        _wordle = new Wordle(6, new MockDataAccess("zzllz"));
+        var res = _wordle.MakeMove("xxxlx");
+
+        LetterFeedback[] expectedResult =
+        [
+            LetterFeedback.IncorrectLetter,
+            LetterFeedback.IncorrectLetter,
+            LetterFeedback.IncorrectLetter,
+            LetterFeedback.CorrectPosition,
+            LetterFeedback.IncorrectLetter,
+        ];
+        Assert.That(res.Feedback, Is.EqualTo(expectedResult));
+    }
+    
+    [Test]
+    public void IncorrectPosition()
+    {
+        _wordle = new Wordle(6, new MockDataAccess("zzzlz"));
+        var res = _wordle.MakeMove("lxxxx");
+
+        LetterFeedback[] expectedResult =
+        [
+            LetterFeedback.IncorrectPosition,
+            LetterFeedback.IncorrectLetter,
+            LetterFeedback.IncorrectLetter,
+            LetterFeedback.IncorrectLetter,
+            LetterFeedback.IncorrectLetter,
+        ];
+        Assert.That(res.Feedback, Is.EqualTo(expectedResult));
+    }
+    
+    [Test]
+    public void AllDifferentFeedbackTypesAtOnce()
+    {
+        _wordle = new Wordle(6, new MockDataAccess("abczz"));
+        var res = _wordle.MakeMove("xxcba");
+
+        LetterFeedback[] expectedResult =
+        [
+            LetterFeedback.IncorrectLetter,
+            LetterFeedback.IncorrectLetter,
+            LetterFeedback.CorrectPosition,
+            LetterFeedback.IncorrectPosition,
+            LetterFeedback.IncorrectPosition,
+        ];
+        Assert.That(res.Feedback, Is.EqualTo(expectedResult));
+    }
 }
 
 public class MockDataAccess : IDataAccess
