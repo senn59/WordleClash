3d29af8ce3c77797cf26f6883756f4483a325d44
senn59 <senn3004@gmail.com>
2024-04-08 14:10:57 +0200

    Seperate logic concerning the chosen word into a different class

--- WordleClash.Core/Game.cs
+++ WordleClash.Core/Game.cs
@@ -6,7 +6,7 @@ namespace WordleClash.Core;
 
 public class Game
 {
-    private readonly string _word;
+    private readonly WordHandler _wordHandler;
     private readonly int _maxTries;
     private readonly IDataAccess _dataAccess;
     
@@ -16,18 +16,17 @@ public class Game
     {
         _dataAccess = dataAccess;
         _maxTries = maxTries;
-        _word = dataAccess.GetRandomWord();
+        _wordHandler = new WordHandler(_dataAccess);
     }
 
     public MoveResult MakeMove(string input)
     {
         ValidateMove(input);
         Tries++;
-        var feedback = GetWordFeedback(input);
         GameStatus status;
         
         //not sure if the 2nd statement is necessary as it shouldnt really be possible anyways
-        if (IsCorrectWord(feedback) && Tries <= _maxTries) 
+        if (_wordHandler.IsMatchingWord(input) && Tries <= _maxTries) 
         {
             status = GameStatus.Won;
         }
@@ -43,72 +42,20 @@ public class Game
         return new MoveResult()
         {
             Status = status,
-            Feedback = feedback
+            Feedback = _wordHandler.GetFeedback(input)
         };
     }
 
     private void ValidateMove(string input)
     {
-        if (input.Length != _word.Length)
+        if (!_wordHandler.IsCorrectLength(input))
         {
-            throw new InvalidWordException($"Word is not the correct length of {_word.Length}");
+            throw new InvalidWordException($"Word is not the correct length of {_wordHandler.Word.Length}");
         }
 
-        if (_dataAccess.GetWord(input) == null)
+        if (!_wordHandler.IsExistingWord(input))
         {
             throw new InvalidWordException($"Word is not a valid word");
         }
-    }
-
-    private LetterFeedback[] GetWordFeedback(string input)
-    {
-        var feedback = new LetterFeedback[_word.Length];
-
-        for (int i = 0; i < input.Length; i++)
-        {
-            var letter = input[i];
-            if (!_word.Contains(letter))
-            {
-                feedback[i] = LetterFeedback.IncorrectLetter;
-                continue;
-            }
-
-            if (GetAllIndexesOf(_word, letter).Contains(i))
-            {
-                feedback[i] = LetterFeedback.CorrectPosition;
-                continue;
-            }
-
-            feedback[i] = LetterFeedback.IncorrectPosition;
-        }
-
-        return feedback;
-    }
-
-    private List<int> GetAllIndexesOf(string input, char letter)
-    {
-        var occurences = new List<int>();
-        var index = input.IndexOf(letter);
-        while (index != -1)
-        {
-            occurences.Add(index);
-            index = input.IndexOf(letter, index + 1);
-        }
-
-        return occurences;
-    }
-
-    private bool IsCorrectWord(LetterFeedback[] feedback)
-    {
-
-        //TODO: has flaw that if array is empty true is returned
-        foreach (var letterFeedback in feedback)
-        {
-            if (letterFeedback is LetterFeedback.IncorrectLetter or LetterFeedback.IncorrectPosition)
-            {
-                return false;
-            }
-        }
-        return true;
     }
 }
