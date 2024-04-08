477b675c839887f4a50975fb543d2973800e8557
senn59 <74777070+senn59@users.noreply.github.com>
2024-04-08 11:04:23 +0200

    rename file and class to be more concise

--- WordleClash.Tests/GameStatusTests.cs
+++ WordleClash.Tests/GameStatusTests.cs
@@ -3,7 +3,7 @@ using WordleClash.Core.DataAccess;
 
 namespace WordleClash.Tests;
 
-public class HasWonConditionTests
+public class GameStatusTests
 {
     [Test]
     public void NoCommonLetters()
