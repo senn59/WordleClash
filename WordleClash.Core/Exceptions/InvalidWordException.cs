cc33a9f4d7000c0f1d0eb13c9c86d89110e06453
senn59 <74777070+senn59@users.noreply.github.com>
2024-04-08 14:23:57 +0200

    improve exceptions

--- WordleClash.Core/Exceptions/InvalidWordException.cs
+++ WordleClash.Core/Exceptions/InvalidWordException.cs
@@ -2,5 +2,5 @@ namespace WordleClash.Core.Exceptions;
 
 public class InvalidWordException: Exception
 {
-    public InvalidWordException(string message) : base(message) {}
+    public InvalidWordException(string input) : base($"{input} is not a valid word.") {}
 }
