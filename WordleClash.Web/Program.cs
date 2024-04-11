465c864cd5bea70b94c66ef66e2fb8c1e156b6ea
senn59 <74777070+senn59@users.noreply.github.com>
2024-04-11 11:32:13 +0200

    Add service for managing game instances, does not yet dispose of instances

--- WordleClash.Web/Program.cs
+++ WordleClash.Web/Program.cs
@@ -1,19 +1,28 @@
 using WordleClash.Core;
 using WordleClash.Core.DataAccess;
 using WordleClash.Data;
+using WordleClash.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+builder.Services.AddDistributedMemoryCache();
 
 var connString = builder.Configuration.GetConnectionString("DefaultConnection");
 if (connString == null) throw new ArgumentNullException($"Connection string cannot be null");
 builder.Services.AddScoped<IDataAccess>(s => new DataAccess(connString));
-builder.Services.AddScoped<Game>(s => new Game(6, s.GetRequiredService<IDataAccess>()));
+builder.Services.AddSingleton<GameService>();
+// builder.Services.AddScoped<Game>(s => new Game(6, s.GetRequiredService<IDataAccess>()));
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -28,6 +37,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapRazorPages();
 
