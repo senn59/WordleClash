46d3bca49d46251d949380f83abe430f94ddaaa3
senn59 <senn3004@gmail.com>
2024-04-11 11:32:13 +0200

    Add service for managing game instances, does not yet dispose of instances

--- WordleClash.Web/Pages/Singleplayer.cshtml.cs
+++ WordleClash.Web/Pages/Singleplayer.cshtml.cs
@@ -1,29 +1,51 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using WordleClash.Core;
-using WordleClash.Core.DataAccess;
+using Exception = System.Exception;
 
 namespace WordleClash.Web.Pages;
 
 public class SingleplayerModel : PageModel
 {
-   private readonly ILogger<IndexModel> _logger;
+    private readonly ILogger<IndexModel> _logger;
+    public const string GameSessionKey = "Game";
+    private GameService _gameService;
 
-    private Game _wordle;
+    [BindProperty] public string Guess { get; set; }
     
-    public SingleplayerModel(ILogger<IndexModel> logger, Game game)
+    public SingleplayerModel(ILogger<IndexModel> logger, GameService gameService)
     {
         _logger = logger;
-        _wordle = game;
+        _gameService = gameService;
     }
 
     public void OnGet()
     {
+        var wordle = _gameService.GetOrCreate(GetGameId());
+        _logger.LogInformation("Got the game instance");
     }
 
     public void OnPost()
     {
-        _wordle.MakeMove("table");
-        _logger.LogInformation($"{_wordle.Tries}");
+        var wordle = _gameService.GetOrCreate(GetGameId());
+        _logger.LogInformation("Got the game instance");
+        try
+        {
+            wordle.MakeMove(Guess);
+            _logger.LogInformation($"user guesses {Guess}");
+        }
+        catch (Exception e)
+        {
+            _logger.LogWarning($"{e.GetType()} thrown while trying to make move.");
+        }
+        _logger.LogInformation($"{wordle.Tries}");
+    }
+
+    private string GetGameId()
+    {
+        if (string.IsNullOrEmpty(HttpContext.Session.GetString(GameSessionKey)))
+        {
+            HttpContext.Session.SetString(GameSessionKey, Guid.NewGuid().ToString());
+        }
+        return HttpContext.Session.GetString(GameSessionKey) ?? throw new NullReferenceException("Couldnt find gameId");
     }
 }
