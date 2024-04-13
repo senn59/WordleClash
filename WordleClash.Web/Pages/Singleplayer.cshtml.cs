a705e2addf49de5bca3d2f36ad72934d64e06d82
senn59 <74777070+senn59@users.noreply.github.com>
2024-04-13 18:48:25 +0200

    integrate new game state logic, create logic and basic UI for starting a new game

--- WordleClash.Web/Pages/Singleplayer.cshtml.cs
+++ WordleClash.Web/Pages/Singleplayer.cshtml.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WordleClash.Core;
+using WordleClash.Core.Enums;
+using WordleClash.Web.Services;
 using Exception = System.Exception;
 
 namespace WordleClash.Web.Pages;
@@ -13,8 +15,10 @@ public class SingleplayerModel : PageModel
     private SessionService _sessionService;
 
     [BindProperty] public string Guess { get; set; }
+    [BindProperty] public bool NewGame { get; set; }
     public int Tries { get; set; }
     public int MaxTries { get; set; }
+    public GameStatus Status { get; set; }
     public IReadOnlyList<GuessResult> MoveHistory { get; set; }
     
     public SingleplayerModel(ILogger<IndexModel> logger, GameService gameService, SessionService sessionService)
@@ -28,27 +32,36 @@ public class SingleplayerModel : PageModel
     {
         var id = _sessionService.GetOrCreateGameId(HttpContext, GameSessionKey);
         var wordle = _gameService.GetOrCreate(id);
+        if (wordle.GameStatus == GameStatus.AwaitStart)
+        {
+            wordle.Start(6);
+        }
         Tries = wordle.Tries;
         MaxTries = wordle.MaxTries;
         MoveHistory = wordle.MoveHistory;
-        _logger.LogInformation("Got the game instance");
+        Status = wordle.GameStatus;
+        _logger.LogInformation($"Got game {id}");
     }
 
     public IActionResult OnPost()
     {
         var id = _sessionService.GetOrCreateGameId(HttpContext, GameSessionKey);
+        if (NewGame)
+        {
+            _gameService.DicardInstance(id);
+            return new RedirectToPageResult("Singleplayer");
+        }
         var wordle = _gameService.GetOrCreate(id);
-        _logger.LogInformation("Got the game instance");
+        _logger.LogInformation($"Got game {id} ");
         try
         {
             wordle.TakeGuess(Guess);
-            _logger.LogInformation($"user guesses {Guess}");
+            _logger.LogInformation($"User attempt: {Guess}");
         }
         catch (Exception e)
         {
             _logger.LogWarning($"{e.GetType()} thrown while trying to make move.");
         }
-        _logger.LogInformation($"{wordle.Tries}");
         return new RedirectToPageResult("Singleplayer");
     }
 }
