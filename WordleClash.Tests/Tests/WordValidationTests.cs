2b46040ed7d60d71871fdff035ebb102be0c84e6
senn59 <senn3004@gmail.com>
2024-04-18 08:46:29 +0200

    remove overloading constructor and change tests accordingly

--- WordleClash.Tests/Tests/WordValidationTests.cs
+++ WordleClash.Tests/Tests/WordValidationTests.cs
@@ -9,28 +9,32 @@ public class WordValidationTests
     [Test]
     public void GuessEmptyWord()
     {
-        var wordle = new Game(3, new MockDataAccess("vwxyz", "vwxyz"));
+        var wordle = new Game(new MockDataAccess("vwxyz", "vwxyz"));
+        wordle.Start(3);
         Assert.Throws<IncorrectLengthException>(() => wordle.TakeGuess(""));
     }
     
     [Test]
     public void GuessTooShortWord()
     {
-        var wordle = new Game(3, new MockDataAccess("vwxyz", "vwxyz"));
+        var wordle = new Game(new MockDataAccess("vwxyz", "vwxyz"));
+        wordle.Start(3);
         Assert.Throws<IncorrectLengthException>(() => wordle.TakeGuess("vwxy"));
     }
     
     [Test]
     public void GuessInvalidWord()
     {
-        var wordle = new Game(3, new MockDataAccess("vwxyz", "vwxyz"));
+        var wordle = new Game(new MockDataAccess("vwxyz", "vwxyz"));
+        wordle.Start(3);
         Assert.Throws<InvalidWordException>(() => wordle.TakeGuess("abcde"));
     }
     
     [Test]
     public void GuessValidWord()
     {
-        var wordle = new Game(3, new MockDataAccess("vwxyz", "vwxyz"));
+        var wordle = new Game(new MockDataAccess("vwxyz", "vwxyz"));
+        wordle.Start(3);
         Assert.DoesNotThrow(() => wordle.TakeGuess("vwxyz"));
     }
     
