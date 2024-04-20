f9518846c9ce821b15ebac3bf82973d60e6a9b66
senn59 <senn3004@gmail.com>
2024-04-20 18:27:51 +0200

    prepare session service for creating players and lobby

--- WordleClash.Web/Pages/Lobby/Create.cshtml.cs
+++ WordleClash.Web/Pages/Lobby/Create.cshtml.cs
@@ -1,23 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WordleClash.Core;
+using WordleClash.Web.Services;
 
 namespace WordleClash.Web.Pages.Lobby;
 
 public class CreateModel : PageModel
 {
     private ILogger<CreateModel> _logger;
+    private SessionService _sessionService;
+    private LobbyService _lobby;
     
     [BindProperty]
     public string Name { get; set; }
 
-    public CreateModel(ILogger<CreateModel> logger)
+    public CreateModel(ILogger<CreateModel> logger, LobbyService lobby, SessionService sessionService)
     {
         _logger = logger;
+        _sessionService = sessionService;
+        _lobby = lobby;
     }
     public void OnGet() {}
 
     public void OnPost()
     {
+        var player = new Player()
+        {
+            Name = Name
+        };
+        _sessionService.SetPlayerSession(player.Id);
+        var code = _lobby.Create("", player);
+        _sessionService.SetLobbySession(code);
         _logger.LogInformation(Name);
     }
 }
