5cd354726a1df0af828f6d90e56abde2caf80607
senn59 <74777070+senn59@users.noreply.github.com>
2024-05-09 15:47:21 +0200

    add wordle field and letteroverview as partial pages

---
+++ WordleClash.Web/Pages/Shared/LetterOverview.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WordleClash.Web.Pages.Shared;
+
+public class LetterOverview : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
