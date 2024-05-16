9099ec664583b6571d81727864929e7cd7f75432
senn59 <74777070+senn59@users.noreply.github.com>
2024-05-16 15:01:45 +0200

    name refactoring

--- WordleClash.Core/GameView.cs
+++ WordleClash.Core/GameView.cs
@@ -7,7 +7,7 @@ public class GameView
     public int Tries { get; set; }
     public int MaxTries { get; set; }
     public GameStatus Status { get; set; }
-    public IReadOnlyList<GuessResult> MoveHistory { get; set; }
+    public IReadOnlyList<GuessResult> GuessHistory { get; set; }
     public static GameView FromGame(Game game)
     {
         return new GameView()
@@ -15,7 +15,7 @@ public class GameView
             Tries = game.Tries,
             MaxTries = game.MaxTries,
             Status = game.Status,
-            MoveHistory = game.MoveHistory
+            GuessHistory = game.GuessHistory
         };
     }
 }
