ba117e1022d006d9fa9bb0f7cec9f25f2b95662a
senn59 <senn3004@gmail.com>
2024-05-17 19:34:35 +0200

    rename GameView -> GameModel

--- WordleClash.Web/Pages/Shared/Field.cshtml.cs
+++ WordleClash.Web/Pages/Shared/Field.cshtml.cs
@@ -4,6 +4,6 @@ namespace WordleClash.Web.Pages.Shared;
 
 public class FieldModel
 {
-    public GameView Game { get; set; }
+    public GameModel Game { get; set; }
     public bool HighlightTiles { get; set; } = false;
 }
