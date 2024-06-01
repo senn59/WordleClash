a04600ab2105a1571010b12b744790cad3600470
senn59 <senn3004@gmail.com>
2024-06-01 14:44:27 +0200

    add checks for singleplayer game state in middlware

--- WordleClash.Web/Pages/Index.cshtml.cs
+++ WordleClash.Web/Pages/Index.cshtml.cs
@@ -1,31 +1,17 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using WordleClash.Core;
-using WordleClash.Web.Services;
 
 namespace WordleClash.Web.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-    private GameService _gameService;
-    private SessionService _sessionService;
 
-    public IndexModel(ILogger<IndexModel> logger, GameService gameService, SessionService sessionService)
+    public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
-        _gameService = gameService;
-        _sessionService = sessionService;
     }
 
-    public IActionResult OnGet()
+    public void OnGet()
     {
-        //TODO change into middleware or caching
-        var gameSession = _sessionService.GetGameId();
-        if (gameSession == null) return Page();
-        _logger.LogInformation($"Game instance found, discarding {gameSession}");
-        _gameService.DicardInstance(gameSession);
-        _sessionService.ClearGameId();
-        return Page();
     }
 }
