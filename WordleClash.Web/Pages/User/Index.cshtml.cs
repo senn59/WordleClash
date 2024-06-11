5722470234e9327fecbdd968a5f74c4f002cdcbb
senn59 <74777070+senn59@users.noreply.github.com>
2024-06-11 23:44:42 +0200

    create basic flow for creating account

--- WordleClash.Web/Pages/User/Index.cshtml.cs
+++ WordleClash.Web/Pages/User/Index.cshtml.cs
@@ -1,28 +1,49 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WordleClash.Core;
 using WordleClash.Web.Services;
 
-namespace WordleClash.Web.Pages.Profile;
+namespace WordleClash.Web.Pages.User;
 
 public class IndexModel : PageModel
 {
     private ILogger<IndexModel> _logger;
     private SessionManager _sessionManager;
+    private UserService _userService;
+    
+    [BindProperty]
+    public new Core.User User { get; set; }
 
-    public IndexModel(ILogger<IndexModel> logger, SessionManager sessionManager)
+    public IndexModel(ILogger<IndexModel> logger, SessionManager sessionManager, UserService userService)
     {
         _logger = logger;
         _sessionManager = sessionManager;
+        _userService = userService;
     }
 
-    public void OnGet()
+    public IActionResult OnGet(string username)
     {
         _logger.LogInformation("Trying to retrieve account");
+        var user = _userService.Get(username);
+        if (user == null)
+        {
+            return RedirectToPage("/Index");
+        }
+
+        User = user;
+        return Page();
     }
 
     public IActionResult OnPostCreate()
     {
         _logger.LogInformation("Trying to create account");
+        if (_sessionManager.GetUserSession() != null)
+        {
+            return RedirectToPage("/Index");
+        }
+        var result = _userService.Create();
+        Console.WriteLine(result.UserName);
+        _sessionManager.SetUserSession(result.SessionId);
         return RedirectToPage("Index");
     }
 }
