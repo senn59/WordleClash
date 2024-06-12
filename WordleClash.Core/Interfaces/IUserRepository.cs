5507146ee3a68338bece0a573d699b1726c39fc5
senn59 <senn3004@gmail.com>
2024-06-12 11:28:20 +0200

    add back function for getting user by sessionId

--- WordleClash.Core/Interfaces/IUserRepository.cs
+++ WordleClash.Core/Interfaces/IUserRepository.cs
@@ -4,5 +4,6 @@ public interface IUserRepository
 {
    CreateUserResult Create(string sessionId);
    User GetByName(string name);
+   User GetFromSessionId(string sessionId);
    void DeleteBySessionId(string sessionId);
 }
