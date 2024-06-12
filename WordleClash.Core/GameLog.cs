fbd2a5bdb6ceb230dd67cce5880c205cfa43f139
senn59 <74777070+senn59@users.noreply.github.com>
2024-06-12 13:34:47 +0200

    make changes to gamelog

--- WordleClash.Core/GameLog.cs
+++ WordleClash.Core/GameLog.cs
@@ -2,9 +2,8 @@ namespace WordleClash.Core;
 
 public class GameLog
 {
-    public int Id { get; init; }
-    public int AttemptCount { get; init; }
-    public TimeSpan Time { get; init; }
-    public bool Status { get; init; }
-    public string Word { get; init; }
+    public required int AttemptCount { get; init; }
+    public required TimeSpan? Time { get; init; }
+    public required bool Status { get; init; }
+    public required string Word { get; init; }
 }
