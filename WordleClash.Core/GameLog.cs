a0016a5cbdded752f8bea7d7bd0d9688f67f7750
senn59 <74777070+senn59@users.noreply.github.com>
2024-06-13 11:46:12 +0200

    create method for humanized time difference, change Status type

--- WordleClash.Core/GameLog.cs
+++ WordleClash.Core/GameLog.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using WordleClash.Core.Enums;
 using WordleClash.Core.Exceptions;
 
@@ -7,7 +8,8 @@ public class GameLog
 {
     public required int AttemptCount { get; init; }
     public required TimeSpan? Time { get; init; }
-    public required int Status { get; init; }
+    public required GameStatus Status { get; init; }
+    public DateTime Date { get; init; } = DateTime.Now.AddHours(-15);
     public required string Word { get; init; }
 
     public static GameLog FromGame(Game game)
@@ -21,8 +23,27 @@ public class GameLog
         {
             AttemptCount = game.Tries,
             Time = null,
-            Status = (int)game.Status,
+            Status = game.Status,
             Word = game.GetTargetWord() ?? string.Empty //only null if game is still in progress
+        };
+    }
+
+    public static string GetTimeDifference(DateTime date)
+    {
+        const int second = 1;
+        const int minute = 60 * second;
+        const int hour = 60 * minute;
+        const int day = 24 * hour;
+        const int month = 30 * day;
+        const string suffix = "ago";
+        var difference = DateTime.Now.Subtract(date);
+        return difference.TotalSeconds switch
+        {
+            < minute => $"{(int)difference.TotalSeconds}s {suffix}",
+            < hour => $"{(int)difference.TotalMinutes}m {suffix}",
+            < day => $"{(int)difference.TotalHours}h {suffix}",
+            < month => $"{(int)difference.TotalDays}d {suffix}",
+            _ => $"{(int)difference.TotalDays / 365}y ago"
         };
     }
 }
