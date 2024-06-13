59ad361db2ce5dd8bf8a98059eecc8fb18c5cf41
senn59 <74777070+senn59@users.noreply.github.com>
2024-06-13 08:50:30 +0200

    add method for creating GameLog object

--- WordleClash.Core/GameLog.cs
+++ WordleClash.Core/GameLog.cs
@@ -1,9 +1,27 @@
+using WordleClash.Core.Enums;
+using WordleClash.Core.Exceptions;
+
 namespace WordleClash.Core;
 
 public class GameLog
 {
     public required int AttemptCount { get; init; }
     public required TimeSpan? Time { get; init; }
-    public required bool Status { get; init; }
+    public required int Status { get; init; }
     public required string Word { get; init; }
+
+    public static GameLog FromGame(Game game)
+    {
+        if (game.Status is not GameStatus.Won or GameStatus.Lost)
+        {
+            throw new GameInProgressException();
+        }
+        return new GameLog
+        {
+            AttemptCount = game.Tries,
+            Time = null,
+            Status = (int)game.Status,
+            Word = game.GetTargetWord() ?? string.Empty //only null if game is still in progress
+        };
+    }
 }
