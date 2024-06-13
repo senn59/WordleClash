8b54be32a66f3765604bed2ac07870c425bfbcaa
senn59 <74777070+senn59@users.noreply.github.com>
2024-06-13 08:31:17 +0200

    move username definition responsibility from repository to service

--- WordleClash.Core/Interfaces/IUserRepository.cs
+++ WordleClash.Core/Interfaces/IUserRepository.cs
@@ -2,7 +2,7 @@ namespace WordleClash.Core.Interfaces;
 
 public interface IUserRepository
 {
-   CreateUserResult Create(string sessionId);
+   CreateUserResult Create(string sessionId, string username);
    User GetByName(string name);
    User GetFromSessionId(string sessionId);
    void DeleteBySessionId(string sessionId);
