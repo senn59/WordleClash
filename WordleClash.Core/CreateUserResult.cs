11e4e5d86b2e98f30685617e3c063e12a3fee41b
senn59 <74777070+senn59@users.noreply.github.com>
2024-06-13 10:07:55 +0200

    add required keyword where necessary

--- WordleClash.Core/CreateUserResult.cs
+++ WordleClash.Core/CreateUserResult.cs
@@ -2,6 +2,6 @@ namespace WordleClash.Core;
 
 public class CreateUserResult
 {
-     public string SessionId { get; init; }
-     public string Username { get; init; }
+     public required string SessionId { get; init; }
+     public required string Username { get; init; }
 }
--- WordleClash.Core/LetterResult.cs
+++ WordleClash.Core/LetterResult.cs
@@ -4,6 +4,6 @@ namespace WordleClash.Core;
 
 public class LetterResult
 {
-    public char Letter { get; init; }
-    public LetterFeedback Feedback { get; init; }
+    public required char Letter { get; init; }
+    public required LetterFeedback Feedback { get; init; }
 }
