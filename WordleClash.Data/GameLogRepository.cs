0ce1fd26cb374af8ee28b36041c926f7cc8f1873
senn59 <74777070+senn59@users.noreply.github.com>
2024-06-19 14:13:07 +0200

    add and implement method for retrieving logs

--- WordleClash.Data/GameLogRepository.cs
+++ WordleClash.Data/GameLogRepository.cs
@@ -1,6 +1,7 @@
 using MySql.Data.MySqlClient;
 using WordleClash.Core;
 using WordleClash.Core.Entities;
+using WordleClash.Core.Enums;
 using WordleClash.Core.Interfaces;
 
 namespace WordleClash.Data;
@@ -9,14 +10,51 @@ public class GameLogRepository: IGameLogRepository
 {
     private readonly string _connString;
     private const string GameLogTable = "game_log";
+    private const int ResultsPerPage = 10;
     public GameLogRepository(string connString)
     {
         _connString = connString;
     }
     
-    public List<GameLog> GetFromUserId(int userId)
+    public List<GameLog> GetFromUserIdByPage(int userId, int pageSize, int page)
     {
-        throw new NotImplementedException();
+        var logs = new List<GameLog>();
+        try
+        {
+            using var conn = new MySqlConnection(_connString);
+            conn.Open();
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = $"SELECT gl.*, w.entry FROM {GameLogTable} as gl" +
+                              "INNER JOIN word AS w ON gl.word_id = w.id" +
+                              "WHERE user_id=@userId " +
+                              "ORDER BY id DESC" +
+                              "LIMIT @pageSize OFFSET @page";
+            cmd.Parameters.AddWithValue("@pageSize", pageSize);
+            cmd.Parameters.AddWithValue("@page", pageSize * (page - 1));
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                TimeSpan? timeVal = null;
+                if (!reader.IsDBNull(reader.GetOrdinal("time")))
+                {
+                    timeVal = reader.GetTimeSpan("time");
+                }
+                logs.Add(new GameLog
+                {
+                    AttemptCount = reader.GetInt32("attempt_count"),
+                    Date = reader.GetDateTime("timestamp"),
+                    Status = (GameStatus)reader.GetByte("status"),
+                    Time = timeVal,
+                    Word = reader.GetString("entry")
+                });
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        return logs;
     }
 
     public void AddToUser(GameLog log, string sessionId)
